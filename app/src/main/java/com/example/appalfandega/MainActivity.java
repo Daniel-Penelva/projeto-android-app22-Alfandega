854a647a147356dc3faf92fdd5783a99c1c7a2ab
Daniel Penelva <d4n.andrade@gmail.com>
2023-04-27 03:33:36 -0300

    Animation| Timer| TimerTask

--- app/src/main/java/com/example/appalfandega/MainActivity.java
+++ app/src/main/java/com/example/appalfandega/MainActivity.java
@@ -4,13 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView myImage;
     TextView myText;
+
+    // Animação
+    Animation visivel, invisivel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
 
         // Para deixar somente o espaço ocupado da imagem no xml, mas invisível
         myImage.setVisibility(View.INVISIBLE);
+
+        // Inicializa a animação
+        visivel = new AlphaAnimation(0,1);
+        invisivel = new AlphaAnimation(1,0);
+
+        // Duração do tempo de animação
+        visivel.setDuration(1000);
+        invisivel.setDuration(1000);
     }
 
     public void eventClick(View view){
@@ -37,5 +53,15 @@ public class MainActivity extends AppCompatActivity {
             myText.setText("Siga para a esquerda!");
             myImage.setScaleX(1f);
         }
+
+        myImage.startAnimation(visivel);
+
+        // Classe Timer para disparar uma ação que irá sumir com a seta.
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                myImage.startAnimation(invisivel);
+            }
+        }, 2000);
     }
 }
