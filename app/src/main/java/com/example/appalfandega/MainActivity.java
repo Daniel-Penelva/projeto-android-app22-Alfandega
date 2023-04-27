1efd32e5036ea95b6a5f5285785f4d715e3a9554
Daniel Penelva <d4n.andrade@gmail.com>
2023-04-27 03:04:43 -0300

    captura id|evento click|setVisibility|condicionar direção seta

--- app/src/main/java/com/example/appalfandega/MainActivity.java
+++ app/src/main/java/com/example/appalfandega/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.appalfandega;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView myImage;
+    TextView myText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myImage = findViewById(R.id.imageView);
+        myText = findViewById(R.id.textView);
+
+        // Para deixar somente o espaço ocupado da imagem no xml, mas invisível
+        myImage.setVisibility(View.INVISIBLE);
+    }
+
+    public void eventClick(View view){
+
+        // Sorteando a direção aleatóriamente
+        if(Math.random() < 0.5){
+            // Para deixar visível a imagem
+            myImage.setVisibility(View.VISIBLE);
+            myText.setText("Siga para a direita!");
+            myImage.setScaleX(-1f);
+        }else{
+            myImage.setVisibility(View.VISIBLE);
+            myText.setText("Siga para a esquerda!");
+            myImage.setScaleX(1f);
+        }
     }
 }
